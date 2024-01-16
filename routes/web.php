b02fde14388d1f6426b1e39fd436a29d53b77751
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2024-01-16 11:36:44 +0100

    Installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Models\Subscription;
 use Illuminate\Support\Facades\Route;
 
@@ -18,15 +19,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/subscriptions', function () {
-    $subscriptions = Subscription::all();
-    return view('subscriptions.index', [
-        'subscriptions' => $subscriptions,
-    ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/subscriptions', function () {
+        $subscriptions = Subscription::all();
+        return view('subscriptions.index', [
+            'subscriptions' => $subscriptions,
+        ]);
+    });
+
+    Route::get('/subscriptions/{subscription}', function (Subscription $subscription) {
+        return view('subscriptions.show', [
+            'subscription' => $subscription,
+        ]);
+    })->name('subscription');
 });
 
-Route::get('/subscriptions/{subscription}', function (Subscription $subscription) {
-    return view('subscriptions.show', [
-        'subscription' => $subscription,
-    ]);
-})->name('subscription');
+require __DIR__.'/auth.php';
